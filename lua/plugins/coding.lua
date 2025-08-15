28a00e8a862b0afe23e158e1a7559f92db375423
mia <miabennett135@gmail.com>
2025-08-15 02:05:16 -0500

    updated mappings

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -36,11 +36,11 @@ return {
       "saghen/blink.cmp"
     }
   },
-  -- {
-  --   'MeanderingProgrammer/render-markdown.nvim',
-  --   dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-  --   opts = {},
-  -- },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    opts = {},
+  },
   {
     "lervag/vimtex",
     lazy=false,
